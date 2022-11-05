98d62d2bc521dba839551e9ea0874fe891bee6c6
laurent <laurent.blondy1@gmail.com>
2022-11-05 17:42:14 +0100

    setup form

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import { config } from './src/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 export default defineConfig({
    site: config.seo.url,
-   output:'server',
+   output: 'server',
    integrations: [tailwind()],
+   adapter: vercel(),
 });
