9edc5327f135881bbb92d1a0d9d51601898468b0
laurent <laurent.blondy1@gmail.com>
2022-11-10 18:31:07 +0100

    remove compress

--- astro.config.ts
+++ astro.config.ts
@@ -3,22 +3,9 @@ import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import { config } from './src/config';
 
-// https://astro.build/config
-import compress from 'astro-compress';
-
-// https://astro.build/config
 export default defineConfig({
    site: config.production.url,
    output: 'server',
-   integrations: [
-      tailwind(),
-      compress({
-         css: true,
-         html: true,
-         img: true,
-         js: true,
-         svg: true,
-      }),
-   ],
+   integrations: [tailwind()],
    adapter: vercel(),
 });
