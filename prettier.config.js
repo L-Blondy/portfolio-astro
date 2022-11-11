fc97c7c1949232aa48b8382ae0b90c3fdcc2f8e4
laurent <laurent.blondy1@gmail.com>
2022-11-11 09:55:53 +0100

    editor

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   arrowParens: 'always',
+   bracketSameLine: false,
+   bracketSpacing: true,
+   endOfLine: 'lf',
+   jsxSingleQuote: false,
+   printWidth: 80,
+   proseWrap: 'preserve',
+   semi: true,
+   singleQuote: true,
+   tabWidth: 3,
+   trailingComma: 'all',
+   useTabs: false,
+};
